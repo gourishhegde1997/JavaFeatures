2d6844dcb4fb5127fecfa48d3a3eba83b67b1463
Gourisha Hegde <gourishdhegde@gmail.com>
2020-08-15 16:12:21 +0530

    Added log4j

---
+++ JavaFeatures/src/main/java/javafeatures/exceptions/JavaExceptionSample1.java
@@ -0,0 +1,13 @@
+package javafeatures.exceptions;
+
+import org.apache.log4j.Logger;
+
+public class JavaExceptionSample1 {
+	
+	static Logger logger = Logger.getLogger(JavaExceptionSample1.class);
+
+	public static void main(String[] args) {
+		logger.info("Hello world...!");
+	}
+
+}
